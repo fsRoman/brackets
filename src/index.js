4aac5210187ba3c66a42ca84dd5ac89a4dc3326b
Roman <ebengard94@gmail.com>
2021-02-26 14:34:24 +0300

    feat: implement function of check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function removePair(str, bracketsConfig) {
+    if (str.length < 2) {
+      return str;
+    } else {
+      let match = false;
+      bracketsConfig.forEach(v => {
+        if (str.indexOf(v.join('')) >= 0) {
+          str = str.replace(v.join(''), '');
+          match = true;
+        };
+      });
+      return (match == false) ? str : removePair(str, bracketsConfig)
+    };
+  };
+  return !removePair(str, bracketsConfig);
 }
